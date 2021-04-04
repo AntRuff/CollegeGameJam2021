ad49a400a407ecaf2eb7766228525375b4c737ed
Impact19 <Isaiahrivers444@gmail.com>
2021-04-04 01:05:19 -0400

    Wire_Movement
    Not as polished but it allows the wire to be moved around by the and connected to the plug.

--- Unplugged/Assets/Scripts/MouseDragMovement.cs
+++ Unplugged/Assets/Scripts/MouseDragMovement.cs
@@ -12,7 +12,7 @@ public class MouseDragMovement : MonoBehaviour
 
 	private void Start()
 	{
-		plane = new Plane(Vector3.up, new Vector3(0, zOffset, 0)); //Create an invisable plane
+		plane = new Plane(Vector3.up, new Vector3(0, zOffset,0 )); //Create an invisable plane
 	}
 
 	private void OnMouseDrag()
@@ -21,7 +21,8 @@ public class MouseDragMovement : MonoBehaviour
 		if (plane.Raycast(ray, out distance)) // Where on the screen are we dragging to?
 		{
 			RaycastHit lineHit; // Line cast hit data
-			Vector3 destination; // Where are we going?
+			Vector3 destination; // Where are we going? 
+			
 			if (Physics.Linecast(transform.position, ray.GetPoint(distance), out lineHit)) // Is something in our way?
 			{
 				destination = lineHit.point; //If so, move to that something
@@ -29,8 +30,10 @@ public class MouseDragMovement : MonoBehaviour
 			else
 			{
 				destination = ray.GetPoint(distance); //Otherwise, go to the mouse position
-			}
+			} 
 			
+			
+
 			GetComponent<Rigidbody>().velocity = (destination - transform.position) * speed; // Move towards the destination
 		}
 	}
@@ -39,4 +42,6 @@ public class MouseDragMovement : MonoBehaviour
 	{
 		GetComponent<Rigidbody>().velocity = Vector3.zero;
 	}
+
+	
 }
